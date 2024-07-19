3e3bf4d7609256314befc8d7779ff328e5184835
gergusz <gergusz03@proton.me>
2024-07-20 01:49:25 +0200

    add gotten powerups to pool

--- ArchipelagoClient/ItemHandler.cs
+++ ArchipelagoClient/ItemHandler.cs
@@ -13,6 +13,7 @@ namespace ArchipelagoMTD.ArchipelagoClient
             {
                randomPowerup = GameObject.Find("PowerupGenerator").GetComponent<PowerupGenerator>().GetRandom(1)[0];
             } while (randomPowerup.name.Contains("archipelago"));
+            GameObject.Find("PowerupGenerator").GetComponent<PowerupGenerator>().takenPowerups.Add(randomPowerup);
             GameObject.Find("PlayerController").GetComponent<PlayerController>().playerPerks.Equip(randomPowerup);
             UIPatcher.CreateText($"<color=#00FF00>Got powerup: </color>{randomPowerup.name}");
         }
